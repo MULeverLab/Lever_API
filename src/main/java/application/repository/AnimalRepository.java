5a65dd81bd21d9562dd5fe44efcc20321fb97257
Jeff Ruffolo <jarpqd@mail.missouri.edu>
2018-01-11 09:21:20 -0600

    Added animal repository basic queries

--- src/main/java/application/repository/AnimalRepository.java
+++ src/main/java/application/repository/AnimalRepository.java
@@ -1,7 +1,27 @@
 package application.repository;
 
 import application.animal.Animal;
+import application.animal.Genotype;
+import application.animal.PhenotypeBridge;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.Set;
+
 public interface AnimalRepository extends JpaRepository<Animal, Integer> {
+    Optional<Set<Animal>> findAnimalsById(Integer id);
+
+    Optional<Set<Animal>> findAnimalsByGenotype(Genotype genotype);
+
+    Optional<Set<Animal>> findAnimalsByPhenotypeBridgeSetContaining(PhenotypeBridge phenotypeBridge);
+
+    Optional<Set<Animal>> findAnimalsBySex(Integer sex);
+
+    Optional<Set<Animal>> findAnimalsByDateOfBirth(Long dateOfBirth);
+
+    Optional<Set<Animal>> findAnimalsByDateOfDeath(Long dateOfDeath);
+
+    Optional<Set<Animal>> findAnimalsByCauseOfDeath(Integer causeOfDeath);
+
+    Optional<Set<Animal>> findAnimalsBySpecies(Integer species);
 }
