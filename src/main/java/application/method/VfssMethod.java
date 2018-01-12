6772d05423ce8996bbe7beb8859a5a31d676dc05
asampath <sampath.ashwath@gmail.com>
2018-01-12 16:24:53 -0600

    vfss class

--- src/main/java/application/method/VfssMethod.java
+++ src/main/java/application/method/VfssMethod.java
@@ -11,7 +11,23 @@ public class VfssMethod {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer id;
-    private 
+    //Position will either be 1 or 2
+    private Integer position;
+    //Trial Number corresponds to 1-5
+    private Integer trialNumber;
+    //Reviewer type corresponds to R1, R2, Consensus
+    private Integer reviewerType;
+    private Long firstSwallowOnsetFrame;
+    private Long pttEndFrame;
+    private Long ettEndFrame;
+    private Long secondSwallowOnsetFrame;
+    private Long swallowsPer2Seconds;
+    private Long lickOnsetFrame;
+    private Long lickEndFrame;
+    private Long lickRate;
+
+
+
 
     public VfssMethod() {
     }
